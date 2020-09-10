b11a078892478dc81290aef47d5ef0f73d7f9182
tomerOko <tomeroko2@gmail.com>
2020-09-10 16:55:44 +0300

    auth

--- config/passport.js
+++ config/passport.js
@@ -9,24 +9,30 @@ module.exports = function (passport) {
     return re.test(String(email).toLowerCase());
 }
 
+const strategyDetails={
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: '/auth/google/callback',
+}
+
+const createUserObject = (profile) => {
+  return {
+    googleId: profile.id,
+    displayName: profile.displayName,
+    firstName: profile.name.givenName,
+    lastName: profile.name.familyName,
+    image: profile.photos[0].value,
+    email: profile._json.email,
+  }
+}
+
   passport.use(
     new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
-      },
-      async (accessToken, refreshToken, profile, done) => {
+      strategyDetails,
+      async(accessToken, refreshToken, profile, done) => {
    
         console.log(profile._json.email)
-        const newUser = {
-          googleId: profile.id,
-          displayName: profile.displayName,
-          firstName: profile.name.givenName,
-          lastName: profile.name.familyName,
-          image: profile.photos[0].value,
-          email: profile._json.email,
-        }
+        const newUser = createUserObject(profile)
 
         try {
           if(validateEmail(profile._json.email)){
--- routes/auth.js
+++ routes/auth.js
@@ -9,11 +9,18 @@ router.get('/google', passport.authenticate('google', {scope : ['profile' , 'ema
 //@desc google Auth Callback
 //@route GET /auth/google/callback
 router.get("/auth/google/callback" , passport.authenticate('google' , {failureRedirect : '/sorry'}) , (req, res) => {res.redirect('/welcome_bgus')});
-// 
 
+//@desc google is Auth
+//@route GET /auth/google/callback
+router.get("/welcome_BGUs" , (req, res) => {  res.send('welcome_bgus!!')});
 
-router.get("/welcome_bgus" , (req, res) => {  res.send('welcome_bgus!!')});
-router.get("/sorry" , (req, res) => {  res.send('sorry.. not a bgu mail')});
+//@desc google Auth Callback
+//@route GET /auth/google/callback
+router.get("/not_BGUs" , (req, res) => {  res.send('sorry.. not a bgu mail')});
+
+//@desc google Auth Callback
+//@route GET /auth/google/callback
+router.get("not_Auth" , (req, res) => {  res.send('not auth')});
 
 
 // router.get('/dashboard', passport.authenticate('google', {failureRedirect : '/'}) , (req, res) => {  res.redirect('/dashboard')})
